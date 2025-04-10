f86ee38656618fc5391f8a9fd156955d9c2e5191
Peter Mackay <petomackay@gmail.com>
2025-04-10 12:40:17 +0200

    ch1-l1

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Println("Couldn't open the file:")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer f.Close()
+	buf := make([]byte, 8)
+	for {
+	    read, err := f.Read(buf)
+	    if err != nil {
+		if errors.Is(err, io.EOF) {
+		    os.Exit(0)
+		}
+		fmt.Println("error reading file:")
+		fmt.Println(err)
+		os.Exit(1)
+	    }
+	    if read == 0 {
+		break
+	    }
+		fmt.Printf("read: %s\n", string(buf[:read]))
+	}
 }
