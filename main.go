b1b716087109b846c22f8e7848c69adc5cba0adb
Peter Mackay <petomackay@gmail.com>
2025-04-14 10:36:26 +0200

    Refactor using channels

--- main.go
+++ main.go
@@ -15,34 +15,40 @@ func main() {
 		log.Fatalf("Couldn't open the file: %v", err)
 	}
 	defer f.Close()
-	buf := make([]byte, 8)
 
-	var current_line string
-	for {
-		read, err := f.Read(buf)
-		if err != nil {
-			if current_line != "" {
-				// dump any lelftovers
-				fmt.Printf("read: %s\n", current_line)
-			}
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			log.Fatalf("error reading the input file: %v", err)
-		}
-		if read == 0 {
-			break
-		}
-		chunk := string(buf[:read])
-		parts := strings.Split(chunk, "\n")
-                if len(parts) > 1 {
-			// there was a newline in the chunk
-			for _, part := range parts[:len(parts) - 1] {
-			    line := current_line + part
-		            fmt.Printf("read: %s\n", line)
-			    current_line = ""
-			}
-		}
-		current_line += parts[len(parts) - 1]
+        lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <- chan string {
+	lines := make(chan string)
+	go func() {
+		defer close(lines)
+		buf := make([]byte, 8)
+		current_line := ""
+		for {
+			read, err := f.Read(buf)
+			if err != nil {
+				if current_line != "" {
+					// dump any leftovers
+					lines <- current_line
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				log.Fatalf("error reading the input file: %v", err)
+			}
+			chunk := string(buf[:read])
+			parts := strings.Split(chunk, "\n")
+			// min lenth should always be 1, so this is safe
+			for _, part := range parts[:len(parts) - 1] {
+				lines <- current_line + part
+				current_line = ""
+			}
+			current_line += parts[len(parts) - 1]
+		}
+	}()
+	return lines
 }
